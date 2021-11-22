b7770bcc8a0cd03a753a78d8cf77c78eb8a3c978
Matthew Roach <matthewroach@users.noreply.github.com>
2021-11-22 14:02:39 +0000

    Grid Alignment prop
    Update to make block plural - blocks

--- src/layouts/grid/item/index.jsx
+++ src/layouts/grid/item/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GridItem = ({
-	children, desktop, tablet, mobile,
+	children, alignment, desktop, tablet, mobile,
 }) => (
 	<div
 		className="l-grid-item"
+		alignment={alignment}
 		style={{
 			'--grid-column-start--desktop': desktop,
 			'--grid-column-start--tablet': tablet || desktop,
@@ -18,12 +19,14 @@ const GridItem = ({
 
 GridItem.defaultProps = {
 	desktop: '',
+	alignment: 'start',
 	tablet: '',
 	mobile: '',
 };
 
 GridItem.propTypes = {
 	children: PropTypes.any.isRequired,
+	alignment: PropTypes.oneOf(['start', 'center', 'end']),
 	desktop: PropTypes.string,
 	tablet: PropTypes.string,
 	mobile: PropTypes.string,
