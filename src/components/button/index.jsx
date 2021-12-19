0ebbd36878397addc2d2722a11d8396eaf55692e
Matthew Roach <github@matthewroach.me>
2021-12-19 21:45:10 +0000

    Add full width button and basic hover theme defaults

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ children, type, size }) => (
-	<button className={`c-button c-button--${size}`} type={type}>
-		{children}
-	</button>
-);
+const Button = ({
+	children, type, size, fullWidth,
+}) => {
+	const classNames = ['c-button', `c-button--${size}`];
+
+	if (fullWidth) {
+		classNames.push('c-button--full');
+	}
+
+	return (
+		<button className={classNames.join(' ')} type={type}>
+			{children}
+		</button>
+	);
+};
 
 Button.defaultProps = {
 	type: 'button',
 	size: 'medium',
+	fullWidth: false,
 };
 
 Button.propTypes = {
 	children: PropTypes.string.isRequired,
 	type: PropTypes.oneOf(['button', 'submit', 'reset']),
 	size: PropTypes.oneOf(['small', 'medium', 'large']),
+	fullWidth: PropTypes.bool,
 };
 
 export default Button;
